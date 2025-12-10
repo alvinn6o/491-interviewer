551ba0178e77c4db38a698a0874f7dbe6b74d0b0
Brandon Christian <brandonjchristian@gmail.com>
2025-12-10 15:21:48 -0800

    upload page view switching

--- src/app/resume/upload/page.tsx
+++ src/app/resume/upload/page.tsx
@@ -1,18 +1,44 @@
 ﻿"use client";
 import styles from "./test.module.css"
+import { createContext, useState } from "react";
 
 export default function ResumeUpload() {
+
     return (
 
         <main className={styles.centered_column}>
             <h1>Resume Scanning</h1>
             <p className="description">Upload your resume and get a real ATS score that reflects what the big companies are using.</p>
             <Instructions />
-            <UploadBox />
+            <ViewSwitcher />
         </main>
 
 
     )
+}
+
+
+enum UploadPageState {
+    UPLOAD,
+    ADD_JOB_DESC,
+    FEEDBACK    
+}
+
+
+function ViewSwitcher() {
+
+    const [uploadState, setUploadState] = useState(UploadPageState.UPLOAD);
+
+    switch (uploadState) {
+        case UploadPageState.UPLOAD:
+            return (<UploadBox changeState={setUploadState} />);
+
+        case UploadPageState.ADD_JOB_DESC:
+            return (<AddJobDescriptionBox changeState={setUploadState} />);
+
+        case UploadPageState.FEEDBACK:
+            return (<ViewFeedbackBox changeState={setUploadState} />);
+    }
 }
 
 function Instructions() {
@@ -37,10 +63,10 @@ function NumberCircle({ number, description }: { number: string; description: st
     )
 }
 
-function UploadBox() {
+function UploadBox({ changeState }: { changeState: React.Dispatch<React.SetStateAction<UploadPageState>> }) {
 
     const UploadResumeButton = () => {
-        alert("Upload functionality not yet implemented.");
+        changeState(UploadPageState.ADD_JOB_DESC);
     };
 
     return (
@@ -53,5 +79,43 @@ function UploadBox() {
             <p className="sub-description">.pdf or .docx file</p>
         </div>
     )
+}
+
+function AddJobDescriptionBox({ changeState }: { changeState: React.Dispatch<React.SetStateAction<UploadPageState>> }) {
+
+    const AddJobDescButton = () => {
+        changeState(UploadPageState.FEEDBACK);
+    };
+
+    return (
+        <div style={{ outline: '2px solid black', width: '50%' }} className={`${styles.centered_column} rounded`}>
+            <div></div>
+            <h2>Add Job Description</h2>
+            <div className={styles.horizontal_line}></div>
+            <img src="/favicon.ico" alt="icon" width="100" />
+            <button className="orange_button" onClick={AddJobDescButton}>Add Job Description</button>
+            <p className="sub-description">.pdf or .docx file</p>
+        </div>
+    )
+
+}
+
+function ViewFeedbackBox({ changeState }: { changeState: React.Dispatch<React.SetStateAction<UploadPageState>> }) {
+
+    const ViewFeedbackBoxButton = () => {
+        changeState(UploadPageState.UPLOAD);
+    };
+
+    return (
+        <div style={{ outline: '2px solid black', width: '50%' }} className={`${styles.centered_column} rounded`}>
+            <div></div>
+            <h2>View Feedback</h2>
+            <div className={styles.horizontal_line}></div>
+            <img src="/favicon.ico" alt="icon" width="100" />
+            <button className="orange_button" onClick={ViewFeedbackBoxButton}>Close Feedback</button>
+            <p className="sub-description">.pdf or .docx file</p>
+        </div>
+    )
+
 }
 
