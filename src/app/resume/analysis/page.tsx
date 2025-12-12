f9cfa52d3d2ec4f120bec6cd063f00c4d3b75686
Brandon Christian <brandonjchristian@gmail.com>
2025-12-12 10:26:54 -0800

    change page location

--- src/app/resume/analysis/page.tsx
+++
@@ -1,9 +0,0 @@
-﻿export default function ResumeAnalysis() {
-    return (
-
-        <div>
-            Resume Analysis Page. Unused! The analysis is actually triggered on Resume/Upload ;)
-        </div>
-
-    )
-}
