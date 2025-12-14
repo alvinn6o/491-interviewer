71cc1ddcee7006cb7d14e24405d0ad7a5bb806c9
Brandon Christian <brandonjchristian@gmail.com>
2025-12-13 17:20:32 -0800

    db requests for feedback

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,16 +1,28 @@
-import { redirect } from "next/navigation";
-import { auth } from "~/server/auth";
+"use client";
+import { useSession } from "next-auth/react";
 
-export default async function AccountPage() {
-  const session = await auth();
+export default function AccountPage() {
+    const { status } = useSession();
 
-  if (!session?.user) {
-    redirect("/login");
-  }
+    // Redirect to login if not authenticated
+    if (status === "unauthenticated") {
+        window.location.href = "/login";
+        return null;
+    } 
+
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-white p-8">
-      
+          <button className="orange_button" onClick={postFeedback}>Post Feedback</button>
+          <button className="orange_button" onClick={deleteFeedback}>Delete Feedback</button>
     </main>
   );
+}
+
+function postFeedback() {
+
+}
+
+function deleteFeedback() {
+
 }
---
+++ src/app/api/feedback/route.ts
@@ -0,0 +1,29 @@
+﻿import { NextResponse } from "next/server";
+import { db } from "~/server/db";
+
+export async function POST(req: Request) {
+    try {
+        const { user, content } = await req.json();
+
+        // create report
+        const report = await db.user.create(
+            {
+                data: {
+
+                },
+                select: {
+                    id: true
+                }
+            }
+        );
+
+        //return NextResponse.json(report, { status: 201 });
+
+    } catch (err) {
+        console.error("GENERATE FEEDBACK ERROR:", err);
+        return NextResponse.json(
+            { error: "Error." },
+            { status: 500 }
+        );
+    }
+}
