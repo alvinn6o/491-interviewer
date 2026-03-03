a9cdf36b287ba449ef02e66a649ca2cb990d1d5e
Brandon Christian <brandonjchristian@gmail.com>
2026-03-02 16:44:00 -0800

    graph data processing

--- src/app/api/account/visualize/sessionProcess.ts
+++ src/app/api/account/visualize/sessionProcess.ts
@@ -1,9 +1,26 @@
 ﻿//Author: Brandon Christian
 //Date: 3/2/2026
 
+enum GraphType {
+    COUNT_BEHAVIORAL,
+    COUNT_TECHNICAL,
+    SCORE_BEHAVIORAL,
+    SCORE_TECHNICAL
+}
+
+type GraphItem = {
+    type: GraphType,
+    name: string,
+    points: GraphPoint[]
+}
+
+type GraphPoint = {
+    date: number,
+    value: number
+}
 
 
-export async function ProcessSessionsToGraphData(sessionsJSON: any) {
+export async function ProcessSessionsToGraphData(sessions: any) {
 
     //TODO: map session data by date
 
@@ -11,54 +28,105 @@ export async function ProcessSessionsToGraphData(sessionsJSON: any) {
     //sessions per day (by type)
     //average score in feedback categories over time
     //average score in technical sessions over time
-    //average score in resume categories over time
 
     //handle display interval automatically based on total amount of days between first and last session
 
-    const countPointsBehavioral = SessionsToGraphByTypeCount(sessionsJSON, "BEHAVIORAL");
-    const countPointsTechnical = SessionsToGraphByTypeCount(sessionsJSON, "TECHNICAL");
+    const graphItems : GraphItem[] = []
+
+    const gpCountBehavioral = GraphByCount(sessions, "BEHAVIORAL");
+    const gpCountTechnical = GraphByCount(sessions, "TECHNICAL");
+
+    graphItems.push(CreateGraphItem(gpCountBehavioral, "Behavioral Count", GraphType.COUNT_BEHAVIORAL));
+    graphItems.push(CreateGraphItem(gpCountTechnical, "TECHNICAL Count", GraphType.COUNT_TECHNICAL));
+
+
+
+    
 }
 
-//TODO: test
-function SessionsToGraphByTypeCount(sessions: any, type: string) {
-    const ungroupedPoints: any[]= [];
+function CreateGraphItem(graphPoints: GraphPoint[], name: string, type: GraphType) {
+    const item: GraphItem = { type: type, name: name, points: graphPoints };
+    return item;
+}
+
+//filter, group, and count the sessions
+function GraphByCount(sessions: any, type: string) {
+    const filtered = FilterByType(sessions, type);
+    const grouped = GroupByRoundedDate(filtered);
+    const points = GraphPointsByCount(grouped);
+
+    return points;
+}
+
+//retrieve sessions by interview type
+function FilterByType(sessions: any, type: string) {
+    const filtered: any[] = [];
 
     sessions.forEach(
         (session: any) => {
             if (session.type == type) {
-                const sessionPoint = { date: session.startedAt, score: 1 };
-                ungroupedPoints.push(sessionPoint);
+                filtered.push(session);
             }
         }
     );
 
-    const pointsGroupedByDay: Record<number, number> = {};
-
-    ungroupedPoints.forEach(
-        (point: any) => {
-
-            //use only day, month, and year
-            const dateRoundedToDay = RoundDateToDay(point.date);
-
-            pointsGroupedByDay[dateRoundedToDay] = pointsGroupedByDay + point.score;
-            //test;
-        }
-
-    );
-
-    const groupedPoints: any[] = [];
-
-    Object.entries(pointsGroupedByDay).forEach(
-        ([date, count]) => {
-            const sessionPoint = { date: date, score: count };
-            groupedPoints.push(sessionPoint);
-        }
-    );
+    return filtered;
 }
 
 //TODO:
 function RoundDateToDay(date: number) {
     return 0;
 }
+
+//group sessions by date, rounded to day
+function GroupByRoundedDate(sessions: any) {
+    const groupedByRoundedDate: Record<number, any[]> = {};
+
+    sessions.forEach(
+        (session: any) => {
+
+            //use only day, month, and year
+            const dateRoundedToDay = RoundDateToDay(session.startedAt);
+
+            //Add array if it doesnt exist yet
+            if (!(dateRoundedToDay in groupedByRoundedDate))
+                groupedByRoundedDate[dateRoundedToDay] = []
+
+            //Add the session to the list of sessions on that day
+            const sessionsOnDay = groupedByRoundedDate[dateRoundedToDay];
+
+            if (sessionsOnDay)
+                sessionsOnDay.push(session);
+        }
+
+    );
+
+    return groupedByRoundedDate;
+}
+
+//create graph points for each day
+//in this case, count the number of sessions on that dday
+function GraphPointsByCount(sessionsByDate: Record<number, any[]>) {
+
+    const graphPoints: GraphPoint[] = [];
+
+    Object.entries(sessionsByDate).forEach(
+        ([date, sessions]) => {
+
+            const count: number = sessions.length;
+
+            //covnvert back to number
+            //as the key is given to us as a string
+            const graphPoint : GraphPoint = { date: Number(date), value: count };
+            graphPoints.push(graphPoint);
+        }
+    );
+
+    return graphPoints;
+}
+
+
+
+
 
 
