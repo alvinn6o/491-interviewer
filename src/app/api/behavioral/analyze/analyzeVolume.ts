e313440016d2d674b5fa7f59c49c5ee4a987ca79
Brandon Christian <brandonjchristian@gmail.com>
2026-04-08 23:23:09 -0700

    pause analysis

--- src/app/api/behavioral/analyze/analyzeVolume.ts
+++ src/app/api/behavioral/analyze/analyzeVolume.ts
@@ -2,9 +2,6 @@
 // Date: 4/6/2026
 //Check the volume of the audio blob over time
 
-//import { decode } from 'wav-decoder';
-//import { FFmpeg } from '@ffmpeg/ffmpeg';
-//import { AudioContext } from 'web-audio-api';
 import decode from 'audio-decode';
 
 
@@ -29,14 +26,16 @@ export async function GetVolume(blob: Blob) {
 
         //average volumes for each second of audio
         const sampleRate = decodedData.sampleRate;
-        const averageVolumesPerSecond = AverageVolume(volumes, sampleRate)
+        const averageVolumesPerSecond = AverageVolume(volumes, sampleRate, 0.5)
 
         //1. average volumes for each second of audio
         //2. locate sections of audio of being quiet or silent for more than 1s
         //3. provide analysis feedback based on frequency and proportion of silent sections over the whole audio
 
         //for now, simply return the average audio sample
-        return averageVolumesPerSecond;
+        //return averageVolumesPerSecond;
+
+        return [AnalyzeVolume(averageVolumesPerSecond)];
     }
 
     //fail
@@ -46,16 +45,6 @@ export async function GetVolume(blob: Blob) {
 //Convert blob into decoded audio data
 //using audio-decode package
 async function DecodeAudio(blob: Blob) {
-
-    /*
-    //move data from blob to array buffer for processing
-    const arrayBuffer = await blob.arrayBuffer();
-
-    //use audio context to decode the audio into viewable samples
-    const audioCtx = new AudioContext();
-
-    const decodedData = await audioCtx.decodeAudioData(arrayBuffer);*/
-
     const arrayBuffer = await blob.arrayBuffer();
     const decodedData = await decode(arrayBuffer);
         
@@ -63,79 +52,121 @@ async function DecodeAudio(blob: Blob) {
 }
 
 //find average volume of each second of audio data
-function AverageVolume(volumes: Float32Array<ArrayBuffer>, sampleRate: number) {
-    //TODO:
+function AverageVolume(volumes: Float32Array<ArrayBuffer>, sampleRate: number, sampleTime: number) {
+    console.log("Volume length: " + volumes.length + " sample rate: " + sampleRate + " seconds: " + volumes.length / sampleRate);
 
-    return [1];
+    const averageVolumes = [];
+
+    let total = 0;
+    let cur = 0;
+
+    volumes.forEach(
+        (volume) => {
+            total += volume;
+            cur += 1;
+
+            if (cur >= sampleRate * sampleTime) {
+                const avg = total / cur;
+                const trunc = Math.trunc(avg * 1000) / 1000
+                averageVolumes.push(trunc);
+
+                total = 0;
+                cur = 0;
+            }
+        }
+    );
+
+    //one last average 
+    if (cur != 0) {
+        const avg = total / cur;
+        const trunc = Math.trunc(avg * 1000) / 1000
+        averageVolumes.push(trunc);
+    }
+    
+
+    return averageVolumes;
 }
 
-//DEPRECATED:
-//INPUT: audio blob in webm format (from userAudio.tsx)
-//OUTPUT: array of volumes at each sample in the data
-/*
-export async function GetVolumeWEBM(blob: Blob) {
-    //TODO: Error: ffmpeg.wasm does not support nodejs, find alternatie solution
 
-    //init FFMPEG to convert webm to wav
-    const ffmpeg = new FFmpeg();
-    await ffmpeg.load();
+//Analyze volume for periods of quiet
 
-    //convert blob to buffer to get processable data
-    const arrayBuffer = await blob.arrayBuffer();
+//INPUT: array of average volume of samples over certain
+//intervals (currently 0.5s)
 
-    //helper file name when dealing with ffmpeg functions
-    const filename = `input-${Date.now()}.webm`;
+//OUTPUT: numerical score based on number of pauses from 1 to 5
 
-    //use ffmpeg's virtual file system
-    //write the audio data as a file
-    ffmpeg.writeFile(filename, new Uint8Array(arrayBuffer));
+async function AnalyzeVolume(averageVolumes: number[]) {
 
-    //simpler to convert to wav and decode than keep as webm
-    await ffmpeg.exec(['-i', filename, '-f', 'wav', 'output.wav']); 
+    const talkingVolumeThreshold = 0.001;
 
-    //read and decode the wav file
-    const wavData = ffmpeg.readFile('output.wav');
-    const audioData = await decode(wavData);
+    //Sample is "quiet" or "talking" based on talkingVolumeThreshold
+    //A random quiet or loud sample is not part of total samples
+    //unless there are at least two of them in a row
+    //There is also forgiveness for the start and end
+    //of the session where a pause is allowed.
 
-    //use audioContext to create a buffer to store the audio data
-    const offlineAudioContext = new OfflineAudioContext(audioData.numberOfChannels, audioData.length, audioData.sampleRate);
-    const buffer = offlineAudioContext.createBuffer(audioData.numberOfChannels, audioData.length, audioData.sampleRate);
+    let sampleSections: any[] = [];
+    let currentSamples = 0;
+    let inPause = true;
 
-    //assume only one channel exists
-    //even if more channels exist, we simply don't process them
-    buffer.copyToChannel(audioData.channelData[0], 0);
+    averageVolumes.forEach(
+        (volume) => {
 
-    //use to store a list of volumes at each frame
-    const volumeData = [];
+            if (volume <= talkingVolumeThreshold) {
 
-    //get the volume of each sample
-    for (let i = 0; i < buffer.length; i++) {
-        const sampleValue = buffer.getChannelData(0)[i];
+                //Add or switch if not in pause
+                if (inPause) {
+                    currentSamples++;
+                }
+                else {
+                    sampleSections.push(currentSamples);
+                    currentSamples = 0;
+                    inPause = true;
+                }
 
-        if (sampleValue !== undefined) {
-            const peakVolume = Math.abs(sampleValue);
-            volumeData.push(peakVolume);
+            }
+            else {
+
+                //switch if in pause or add
+                if (inPause) {
+                    sampleSections.push(currentSamples);
+                    currentSamples = 0;
+                    inPause = false;
+                }
+                else {
+                    currentSamples++;
+                }
+            }
+            
         }
+    );
+
+
+    let preTalkSamples = sampleSections[0];
+    let pauseSamples = 0;
+    let talkSamples = 0;
+    let postTalkSamples = sampleSections[sampleSections.length - 1];
+
+
+    //alternate between pause and talk
+    //assume we start with talk
+    for (let i = 1; i < sampleSections.length - 1; i++) {
+        if (i % 2 == 1) {
+            talkSamples += sampleSections[i];
+        }
+        else {
+            pauseSamples += sampleSections[i];
+        }
+
     }
 
-    return volumeData;
-}
-*/
-//Analyze the volume over time and provide
-//feedback about the volume
+    const totalValidSamples = averageVolumes.length - preTalkSamples - postTalkSamples;
+    const score = (pauseSamples + talkSamples) / totalValidSamples;
 
-//criteria:
-//1. not too loud
-//2. not too quiet
-//3. not too many or too lengthy pauses
+    console.log("pre: " + preTalkSamples + "pause: " + pauseSamples + "talk: " + talkSamples + "post: " + postTalkSamples + " score: " + score);
 
-//what length is considered for a pause?
-//pauses at start or end are OK but mentioned
-//IE: start speaking promptly after beginning a session!
-//ensure to end the session promptly when finished!
+    //TODO: score by threshold, what % of the interview should they be talking at least? 
+    //TODO: penalty for unusually large pause sections
 
-//INPUT: 
-
-async function AnalyzeVolume(volumeData: number[]) {
-
+    return score;
 }
